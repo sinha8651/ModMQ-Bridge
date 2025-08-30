46f50bf719b667ca06ab78d41b98518bc7dc8b64
Rahul Kumar Sinha <rahulsinha8405@gmail.com>
2025-08-30 19:04:20 +0530

    Rebase

---
+++ src/main/java/com/application/close/ModMqBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.application.close;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModMqBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModMqBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/close/ModMqBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.close;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModMqBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
